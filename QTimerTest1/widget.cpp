a16b87e61db94c2d7d3d0520bbfcc3b3a305d0eb
luchun <luchunabf@163.com>
2024-04-05 16:54:14 +0800

    定时器 QTimer

---
+++ QTimerTest1/widget.cpp
@@ -0,0 +1,115 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#define TIMEINTERVAL 2000
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    timerId = 0;
+    pictureId = 1;
+    timer = new QTimer(this); // 要不要this 要
+
+    ui->label->setScaledContents(true);
+    ui->label->setPixmap(QPixmap(":/1.jpg"));
+//    ui->label->setPixmap(QPixmap("file:///C:/Users/86177/Desktop/照片墙/IMG_20240324_194834.jpg"));
+//    ui->label->setPixmap(QPixmap("C:/Users/86177/Desktop/test.jpg"));
+
+
+    connect(ui->startTimerButton, &QPushButton::clicked, this, &Widget::startButtonHandle);
+    connect(ui->stopTimerButton, &QPushButton::clicked, this, &Widget::stopButtonHandle);
+
+    connect(timer, &QTimer::timeout, this, &Widget::timerHandle);
+}
+
+void Widget::startButtonHandle()
+{
+    timer->start(TIMEINTERVAL);
+    qDebug() << "QTimer定时器启动成功";
+}
+
+
+void Widget::stopButtonHandle()
+{
+    timer->stop();
+    qDebug() << "QTimer定时器停止成功";
+}
+
+void Widget::timerHandle()
+{
+    qDebug() << "QTimer定时器运行中...";
+
+    if(pictureId > 4)
+        pictureId = 1;
+    QString s = ":/";
+    s += QString::number(pictureId);
+    s += ".jpg";
+    ui->label->setPixmap(QPixmap(s));
+
+    pictureId++;
+
+
+//    QStringList list;
+//    for(int i = 1; i <= 4; ++i)
+//    {
+//        QString s = ":/";
+//        s += QString::number(i);
+//        s += ".jpg";
+//        list.append(s);
+//        ui->label->setPixmap(list.at(i));
+//    }
+
+//    ui->label->setPixmap(list.at(i));
+}
+
+
+
+
+
+
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::timerEvent(QTimerEvent *event)
+{
+    if (event->timerId() == timerId)
+    {
+        qDebug() << "定时器运行中...";
+
+        if(pictureId > 4)
+            pictureId = 1;
+        QString s = ":/";
+        s += QString::number(pictureId);
+        s += ".jpg";
+        ui->label->setPixmap(QPixmap(s));
+
+        pictureId++;
+    }
+}
+
+
+void Widget::on_startPushButton_clicked()
+{
+    timerId = startTimer(TIMEINTERVAL); // 启动定时器
+    if (timerId != 0)
+    {
+        qDebug() << "定时器启动成功";
+    }
+}
+
+
+void Widget::on_stopPushButton_clicked()
+{
+    if (timerId)
+    {
+        killTimer(timerId); // 停止定时器
+        qDebug() << "定时器停止成功";
+    }
+}
+
---
+++ QTimerTest1/widget.h
@@ -0,0 +1,43 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QDebug>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_startPushButton_clicked();
+
+    void on_stopPushButton_clicked();
+
+    void startButtonHandle();
+    void stopButtonHandle();
+
+    void timerHandle();
+
+protected:
+    virtual void timerEvent(QTimerEvent *event);
+
+private:
+    Ui::Widget *ui;
+    int timerId;
+    QTimer * timer;
+    int pictureId;
+
+//private:
+//    QTimer* timer;
+};
+#endif // WIDGET_H
