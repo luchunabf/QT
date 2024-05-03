559a03cb12cf60ea042dbf1126959dbbb4b74a97
luchun <luchunabf@163.com>
2024-05-03 13:59:17 +0800

    将照片存至数据库

--- testMySqlConnect/main.cpp
+++ testMySqlConnect/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include <QSqlDatabase>
+#include <QFile>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -9,9 +11,10 @@ int main(int argc, char *argv[])
 
     //数据库驱动： ("QSQLITE", "QODBC", "QODBC3", "QPSQL", "QPSQL7")
     // QList("QSQLITE", "QMIMER", "QODBC", "QPSQL")
-    qDebug() << "数据库驱动：" << QSqlDatabase::drivers();
+    // qDebug() << "数据库驱动：" << QSqlDatabase::drivers();
 
 
+    /*
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
     db.setHostName("127.0.0.1");
     db.setPort(3306);//是mysql服务器的端口号
@@ -40,7 +43,6 @@ int main(int argc, char *argv[])
     qDebug() << "query.numRowsAffected()" << query.numRowsAffected();
 
     while (query.next()) {
-        /*对查询结果进行输出*/
         qDebug() << query.value("id").toInt() << " " << query.value("name").toString();
 
 
@@ -67,6 +69,62 @@ int main(int argc, char *argv[])
     {
         qDebug() << "插入数据库成功！";
     }
+
+    */
+
+
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setHostName("127.0.0.1");
+    db.setPort(3306);//是mysql服务器的端口号
+    db.setDatabaseName("emarket");
+    db.setUserName("root");
+    db.setPassword("1234");
+    if (!db.open()) {
+        //        qDebug() << QString::fromLocal8Bit("打开失败");
+        qDebug() << QString("打开失败");
+        return 0;
+    }
+    else{
+        //        qDebug() << QString::fromLocal8Bit("打开成功");
+        qDebug() << QString("打开成功");
+    }
+
+
+    QSqlQuery query(db); //关联数据库
+    // QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\11.jpg";
+    // QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\12.jpg";
+    // QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\13.jpg";
+    // QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\31.jpg";
+    // QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\22.jpg";
+    QString photoPath = "E:\\QtDocument\\QT\\testMySqlConnect\\21.jpg";
+    QFile photoFile(photoPath);
+    if(photoFile.exists())
+    {
+        // 存入数据库
+        QByteArray picdata;
+        photoFile.open(QIODevice::ReadOnly);
+        picdata = photoFile.readAll();
+        photoFile.close();
+
+        QVariant var(picdata);
+        QString sql = "update commodity set picture=? where CommodityID=6";
+        query.prepare(sql);
+        query.addBindValue(var);
+        if(!query.exec())
+        {
+            QMessageBox::information(0, QObject::tr("提示"), "照片写入失败");
+        }
+        else
+        {
+            QMessageBox::information(0, QObject::tr("提示"), "照片写入成功");
+        }
+    }
+
+    db.close();
+
+
+
 
 
 
